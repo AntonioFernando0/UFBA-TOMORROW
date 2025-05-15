4abba4efff432922b13bd6d7a7952a3d6c49f040
Antonio Fernando <jufer376@gmail.com>
2025-05-15 09:56:24 -0300

    Formulários
    Aplicando conceitos que foram passados pelo professor da UFBA, Claudio Silva no curso de JavaScript.

--- Atividades/ex03/index.js
+++ Atividades/ex03/index.js
@@ -1,17 +1,30 @@
   
 
 function enviar() {
-  let nome = document.getElementById("nome").value;
+    let nome = document.getElementById("nome").value.trim();
     let idade = document.getElementById("idade").value;
     let email = document.getElementById("email").value;
     let date = document.getElementById("nasc").value
+    
 
     let soLetras = /^[A-Za-zÀ-ÿ\s]+$/
 
     if(nome === "" || !soLetras.test(nome)){
 
-        alert("Erro: Por favor, digite um nome válido (somente letras)")}else{
+        alert("Erro: Por favor, digite um nome válido (somente letras)");
 
-            alert(nome + idade + email + date)
-        }
+        return;
+}
+
+const p = document.createElement("p");
+p.textContent = `Nome: ${nome}, Idade: ${idade}, Email: ${email}, Data de Nascimento: ${date}.`;
+document.getElementById("container").appendChild(p);
+
+//Limpar todos os dados.
+
+    document.getElementById("nome").value = "";
+    document.getElementById("idade").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("nasc").value = "";
+
 }
